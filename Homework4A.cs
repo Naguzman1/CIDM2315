a00f7ffc72b7f94d089891647de86c9da6c0764c
Naguzman1 <111940806+Naguzman1@users.noreply.github.com>
2022-09-21 14:57:23 -0500

    Add files via upload

---
+++ Homework4A.cs
@@ -0,0 +1,21 @@
+﻿namespace Homework4;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("A = ");
+        int first = int.Parse(Console.ReadLine());
+
+        Console.Write("B = ");
+        int second = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("The Largest Number: " + largest(first, second));
+    }
+    static int largest(int a, int b){
+        if(a>b){
+            return a;
+        }else{
+            return b;
+        }
+    }
+}

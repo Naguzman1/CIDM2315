eacfc7d6cda92b09e15fd46217d9131ab668d4ad
Naguzman1 <111940806+Naguzman1@users.noreply.github.com>
2022-09-28 19:43:16 -0500

    Add files via upload

--- Homework4B.cs
+++ Homework4B.cs
@@ -1,37 +1,40 @@
-﻿namespace Homework4B;
+﻿namespace Homework5B;
 class Program
-{  
-    public static void Main() 
 {
-   int N = 5; 
-   string shape; 
-   Console.Write("Input shape: ");
-   shape= Console.ReadLine(); 
-   Console.Write($"N is; {N} shape is "+shape+"\n"); 
-   side(N,shape); 
+public static void Main(string[] args)
+{
+account();
+Console.ReadKey(true);
 }
-
-    static void side(int N,string shape){
-    int i,j,k; 
-    if(shape=="left"){
-   
-       for(i=1;i<=N;i++)
-       {
-    	for(j=1;j<=i;j++)
-    	   Console.Write("*");
-    	Console.Write("\n"); 
-       }
-      }
-    else if(shape=="right"){
-    //if shape is right
-     for (i = 1; i <= N; i++)  
-         {  
-            for (j = 1; j <= N-i; j++)  
-            {Console.Write(" ");} 
-            for (k = 1; k <= i; k++)  
-            {  Console.Write("*");}  
-            Console.WriteLine("");  }
-        }
-    }
+public static bool checkAge(int birth_year)
+{
+int age = 2022 - birth_year;
+if(age >= 18)
+return true;
+else
+return false;
 }
+public static void account()
+{
+    String userName,password,retypedPassword;
+    int birthYear;
+    Console.WriteLine("Enter Your Username:");
+    userName = Console.ReadLine();
+    Console.WriteLine("Enter Your Password:");
+    password = Console.ReadLine();
+    Console.WriteLine("Enter Your Password Again:");
+    retypedPassword = Console.ReadLine();
+    Console.WriteLine("Enter Your Birthyear:");
+    birthYear = Convert.ToInt32(Console.ReadLine());
 
+    if(checkAge(birthYear))
+{
+    if(password == retypedPassword)
+        Console.WriteLine("Account is created successfully");
+    else
+        Console.WriteLine("Wrong password");
+}
+    else
+        Console.WriteLine("Could not create an account.");
+}
+}

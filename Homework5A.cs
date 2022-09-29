cd6a47333ef18495b11ad395a83d5e592990bb97
Naguzman1 <111940806+Naguzman1@users.noreply.github.com>
2022-09-28 19:42:18 -0500

    Add files via upload

---
+++ Homework5A.cs
@@ -0,0 +1,47 @@
+﻿namespace Homework5A;
+class Program
+{
+    static void Main()
+{
+    int L2 = largest2();
+        Console.Write("The largest number is: "+L2);
+    int L4 = largest4();
+        Console.Write("The largest number is: "+L4);
+}
+    public static int largest2()
+{
+    int a, b, large;
+    Console.WriteLine("Enter 2 numbers: ");
+    a = Convert.ToInt32(Console.ReadLine());
+    b = Convert.ToInt32(Console.ReadLine());
+
+    if(a>b)
+        large = a;
+    else
+        large = b;
+    return large;
+}
+
+public static int largest2Modified(int a, int b)
+{
+    int large;
+
+    if(a>b)
+        large = a;
+    else
+        large = b;
+    return large;
+}
+
+public static int largest4()
+{
+    int a, b, c, d, large;
+    Console.WriteLine("\nEnter 4 numbers: ");
+    a = Convert.ToInt32(Console.ReadLine());
+    b = Convert.ToInt32(Console.ReadLine());
+    c = Convert.ToInt32(Console.ReadLine());
+    d = Convert.ToInt32(Console.ReadLine());
+    large = largest2Modified(largest2Modified(a, b), largest2Modified(c, d));
+    return large;
+}
+}
